9c1ae3b94112287932732016dcb744f144ef33b8
ErdemGKSL <zahrakon06@gmail.com>
2023-03-08 06:30:07 +0300

    .

--- plugins/ErdemGKSL/default-guild-config/src/index.js
+++ plugins/ErdemGKSL/default-guild-config/src/index.js
@@ -1,9 +1,10 @@
 import { subscriptions, persist } from "@acord/extension";
 import { GuildNotificationsActions, FluxDispatcher } from "@acord/modules/common";
+
 async function listener({ guild }) {
   const data = { /* suppress_everyone: true, suppress_roles: true, */ muted: true };
-    for (let key in persist.ghost.settings) if (persist.ghost.settings[key]) data[key] = !!persist.ghost.settings[key];
-    GuildNotificationsActions.updateGuildNotificationSettings(guild.id, data);
+  for (let key in persist.ghost.settings) if (persist.ghost.settings[key]) data[key] = !!persist.ghost.settings[key];
+  GuildNotificationsActions.updateGuildNotificationSettings(guild.id, data);
 }
 
 export default {
@@ -11,5 +12,5 @@ export default {
     FluxDispatcher.subscribe("GUILD_CREATE", listener);
     subscriptions.push(() => FluxDispatcher.unsubscribe("GUILD_CREATE", listener));
   },
-  unload() {}
+  unload() { }
 }
