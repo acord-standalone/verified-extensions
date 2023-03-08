a7b338313be7be3f287920222df0896cabfe64e1
ErdemGKSL <zahrakon06@gmail.com>
2023-03-08 06:29:39 +0300

    .

---
+++ plugins/ErdemGKSL/default-guild-config/dist/readme.md
@@ -0,0 +1,3 @@
+# Default Guild Notifications Config
+
+This plugin allows you to configure notifications for your guilds. You can configure notifications for the next guilds that you join.
---
+++ plugins/ErdemGKSL/default-guild-config/src/index.js
@@ -0,0 +1,15 @@
+import { subscriptions, persist } from "@acord/extension";
+import { GuildNotificationsActions, FluxDispatcher } from "@acord/modules/common";
+async function listener({ guild }) {
+  const data = { /* suppress_everyone: true, suppress_roles: true, */ muted: true };
+    for (let key in persist.ghost.settings) if (persist.ghost.settings[key]) data[key] = !!persist.ghost.settings[key];
+    GuildNotificationsActions.updateGuildNotificationSettings(guild.id, data);
+}
+
+export default {
+  load() {
+    FluxDispatcher.subscribe("GUILD_CREATE", listener);
+    subscriptions.push(() => FluxDispatcher.unsubscribe("GUILD_CREATE", listener));
+  },
+  unload() {}
+}
