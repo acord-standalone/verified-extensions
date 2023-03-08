cb07957eac2722093664f4492007a6dbb0e548dd
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2023-03-08 15:45:33 +0300

    a

---
+++ update-index.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const path = require("path");
+
+const result = [];
+function loadExtensions(type) {
+  fs.readdirSync(path.resolve(__dirname, `./${type}`), { withFileTypes: true }).forEach((authorDir) => {
+    if (!authorDir.isDirectory()) return;
+    fs.readdirSync(path.resolve(__dirname, `./${type}`, authorDir.name)).forEach((extensionId) => {
+      try {
+        const cfg = JSON.parse(fs.readFileSync(path.resolve(__dirname, `./${type}`, authorDir.name, extensionId, "./acord.cfg"), "utf-8"));
+        delete cfg.config;
+        delete cfg.i18n;
+        let outDir = cfg.out.directory.replace(/^(\.\/|\/)?/, "").replace(/\/$/, "");
+        if (fs.existsSync(path.resolve(__dirname, `./${type}`, authorDir.name, extensionId, cfg.out.directory, "./source.js"))) {
+          delete cfg.out;
+          result.push({
+            meta: {
+              author: authorDir.name,
+              id: extensionId,
+              url: `https://raw.githubusercontent.com/acord-standalone/verified-extensions/main/plugins/${authorDir.name}/${extensionId}/${outDir}`
+            },
+            data: cfg
+          })
+        }
+      } catch (err) { console.error(err); }
+    });
+  });
+}
+
+loadExtensions("plugins");
+loadExtensions("themes");
+
+fs.writeFileSync(
+  path.resolve(__dirname, "./index.json"),
+  JSON.stringify(result, null, 2),
+  "utf-8"
+);
