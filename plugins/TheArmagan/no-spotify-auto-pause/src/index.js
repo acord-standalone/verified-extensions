4569cc7496c320d9ea7e273374599d2d6944fb32
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2023-03-08 22:30:14 +0300

    no spotify extension

---
+++ plugins/TheArmagan/no-spotify-auto-pause/src/index.js
@@ -0,0 +1,19 @@
+import patcher from "@acord/patcher";
+import { subscription } from "@acord/extension";
+
+export default {
+  load() {
+    subscription.push(
+      patcher.instead(
+        "open",
+        XMLHttpRequest.prototype,
+        async function (args, instead) {
+          if (args[0] == "PUT" && args[1] == "https://api.spotify.com/v1/me/player/pause") {
+            args[1] = "https://discord.com/";
+          }
+          return instead.call(this, ...args);
+        }
+      )
+    );
+  }
+}
