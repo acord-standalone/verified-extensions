a8b23ba5223b6dc1d87c50d018fe921c039cb1de
ErdemGKSL <zahrakon06@gmail.com>
2023-03-18 11:07:57 +0300

    goodwords

---
+++ plugins/ErdemGKSL/goodword/src/index.js
@@ -0,0 +1,36 @@
+import { subscriptions, persist } from "@acord/extension";
+import { MessageActions } from "@acord/modules/common";
+import { patcher } from "@acord";
+
+const noCharChar = '󠇰';
+
+export default {
+  load() {
+    subscriptions.push(
+      patcher.instead(MessageActions, "sendMessage", (args, original) => {
+
+        const [channelId, message] = args;
+  
+        const words = persist.ghost?.settings?.badwords?.split(`,`).map(word => word.trim());
+  
+        if (!words?.length) return original(...args);
+  
+        let content = message.content.split(" ");
+  
+        if (message.content && message.content.length < 1000) {
+          words.forEach(word => {
+            if (message.content.length < 2000) {
+              content = content.map(part => part.toLowerCase() === word.toLowerCase() ? part.split('').join(noCharChar) : part);
+            }
+          });
+        }
+  
+        message.content = content.join(' ');
+        args[1] = message;
+  
+        return original(...args);
+      })
+    )
+  },
+  unload() {}
+}
