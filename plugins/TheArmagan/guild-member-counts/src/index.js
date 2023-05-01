c455de4e12e4ac1ce13c852f14a281de1eff8186
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2023-05-01 22:54:23 +0300

    member counts!

---
+++ plugins/TheArmagan/guild-member-counts/src/index.js
@@ -0,0 +1,26 @@
+import { subscriptions, i18n } from "@acord/extension";
+import dom from "@acord/dom";
+import utils from "@acord/utils";
+import { GuildMemberCountStore } from "@acord/modules/common";
+
+export default {
+  load() {
+    subscriptions.push(
+      dom.patch(
+        '.headerContent-2SNbie.primaryInfo-2ocY3v [data-text-variant="text-md/semibold"]',
+        /** @param {Element} elm */(elm) => {
+          let guild = utils.react.getProps(elm, i => i?.guild)?.guild;
+          if (!guild) return;
+
+          let countElm = dom.parse(`
+            <div style="opacity: 0.85; font-size: 12px; line-height: 10px; font-weight: 100;">
+              ${i18n.format("MEMBER_COUNT", GuildMemberCountStore.getMemberCount(guild.id).toLocaleString())}
+            </div>
+          `);
+
+          elm.appendChild(countElm);
+        }
+      )
+    )
+  }
+}
