b7c71eeb522c9a065c470ea587072ac19943e2ad
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2023-05-07 02:19:51 +0300

    use new api on deleted messages

--- plugins/TheArmagan/see-deleted-messages/src/index.js
+++ plugins/TheArmagan/see-deleted-messages/src/index.js
@@ -1,5 +1,5 @@
 import { subscriptions } from "@acord/extension";
-import patcher from "@acord/patcher";
+import actionHandlers from "@acord/actionHandlers";
 import dom from "@acord/dom";
 import { FluxDispatcher, MessageStore } from "@acord/modules/common";
 
@@ -11,7 +11,7 @@ export default {
         FluxDispatcher.dispatch({
           type: "MESSAGE_DELETE",
           ...value,
-          _original: true,
+          __original__: true,
         });
       });
       deletedMessageData.clear();
@@ -39,83 +39,41 @@ export default {
 
     subscriptions.push(
       dom.patch('[id^="chat-messages-"]', handleDomElement),
-      patcher.after(
-        "_computeOrderedActionHandlers",
-        FluxDispatcher._actionHandlers,
-        function ([actionName]) {
-          if (!(["MESSAGE_DELETE", "MESSAGE_DELETE_BULK"].includes(actionName))) return;
-          let orderedCallbackTokens = this._orderedCallbackTokens || this._computeOrderedCallbackTokens();
-          let actionHandlers = [];
-          for (let i = 0; i < orderedCallbackTokens.length; i++) {
-            let nodeData = this._dependencyGraph.getNodeData(orderedCallbackTokens[i]);
-            let storeName = nodeData.name;
-            let store = nodeData.actionHandler;
-            let actionHandler = store[actionName];
-            if (actionHandler) {
-              if (actionName === "MESSAGE_DELETE" && storeName === "MessageStore") {
-                actionHandlers.push({
-                  name: storeName,
-                  actionHandler(e) {
-                    if (e._original || shouldIgnoreMessage(MessageStore.getMessage(e.channelId, e.id)))
-                      return actionHandler.call(this, e);
-                    // console.log("actionHandler", storeName, actionName, e);
-                    delete e.type;
-                    deletedMessageData.set(e.id, e);
-                    setTimeout(patchVisibleMessages, 0);
-                  },
-                  storeDidChange(e) {
-                    if (e._original) return nodeData.storeDidChange.call(this, e);
-                    // console.log("storeDidChange", storeName, actionName, e);
-                  }
-                });
-              } else if (actionName === "MESSAGE_DELETE_BULK" && storeName === "MessageStore") {
-                actionHandlers.push({
-                  name: storeName,
-                  actionHandler(e) {
-                    if (e._original) return actionHandler.call(this, e);
-                    delete e.type;
-                    let toDelete = [];
-                    e.ids.forEach(id => {
-                      if (shouldIgnoreMessage(MessageStore.getMessage(e.channelId, id)))
-                        return toDelete.push(id);
-                      deletedMessageData.set(e.id, { id, channelId: e.channelId, guildId: e.guildId });
-                    });
-                    if (toDelete.length) {
-                      FluxDispatcher.dispatch({
-                        type: "MESSAGE_DELETE_BULK",
-                        ids: toDelete,
-                        channelId: e.channelId,
-                        guildId: e.guildId,
-                        _original: true,
-                      });
-                    }
-                    setTimeout(patchVisibleMessages, 0);
-                  },
-                  storeDidChange(e) {
-                    if (e._original) return nodeData.storeDidChange.call(this, e);
-                  }
-                });
-              } else {
-                actionHandlers.push({
-                  name: storeName,
-                  actionHandler: actionHandler,
-                  storeDidChange: nodeData.storeDidChange
-                });
-              }
-            }
+      actionHandlers.patch("MESSAGE_DELETE", "MessageStore", {
+        actionHandler(d) {
+          if (!shouldIgnoreMessage(MessageStore.getMessage(d.event.channelId, d.event.id))) d.cancel();
+          delete d.event.type;
+          deletedMessageData.set(d.event.id, d.event);
+          setTimeout(patchVisibleMessages, 0);
+        },
+        storeDidChange(d) {
+          d.cancel();
+        }
+      }),
+      actionHandlers.patch("MESSAGE_DELETE_BULK", "MessageStore", {
+        actionHandler(d) {
+          let toDelete = [];
+          d.event.ids.forEach(id => {
+            if (shouldIgnoreMessage(MessageStore.getMessage(d.event.channelId, id)))
+              return toDelete.push(id);
+            deletedMessageData.set(id, { id, channelId: d.event.channelId, guildId: d.event.guildId });
+          });
+          if (toDelete.length) {
+            FluxDispatcher.dispatch({
+              type: "MESSAGE_DELETE_BULK",
+              ids: toDelete,
+              channelId: d.event.channelId,
+              guildId: d.event.guildId,
+              __original__: true,
+            });
           }
-          this._orderedActionHandlers[actionName] = actionHandlers;
-          return actionHandlers;
+          setTimeout(patchVisibleMessages, 0);
+          d.cancel();
+        },
+        storeDidChange(d) {
+          d.cancel();
         }
-      ),
-      (() => {
-        FluxDispatcher._actionHandlers._computeOrderedActionHandlers("MESSAGE_DELETE");
-        FluxDispatcher._actionHandlers._computeOrderedActionHandlers("MESSAGE_DELETE_BULK");
-        return () => {
-          FluxDispatcher._actionHandlers._computeOrderedActionHandlers("MESSAGE_DELETE");
-          FluxDispatcher._actionHandlers._computeOrderedActionHandlers("MESSAGE_DELETE_BULK");
-        }
-      })()
+      }),
     )
   },
   unload() { }
