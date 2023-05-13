864bafd42637c9e562326d69e83014d3337287e3
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2023-05-14 00:39:28 +0300

    fix dbl click to edit

--- plugins/TheArmagan/double-click-to-edit/src/index.js
+++ plugins/TheArmagan/double-click-to-edit/src/index.js
@@ -1,19 +1,33 @@
-import { MessageStore, MessageActions, UserStore } from "@acord/modules/common";
+import { MessageStore, MessageActions, UserStore, FluxDispatcher } from "@acord/modules/common";
 import { subscriptions } from "@acord/extension";
 import dom from "@acord/dom";
 
 export default {
   load() {
+    function handleElement(e) {
+      const [, , channelId, messageId] = e.id.split('-');
+      const message = MessageStore.getMessage(channelId, messageId);
+      if (!message || message.author.id !== UserStore.getCurrentUser().id) return;
+      e.ondblclick = () => MessageActions.startEditMessage(message.channel_id, message.id, message.content);
+    }
     subscriptions.push(
       dom.patch(
         '[id^="chat-messages-"]',
-        (e) => {
-          const [, , channelId, messageId] = e.id.split('-');
-          const message = MessageStore.getMessage(channelId, messageId);
-          if (!message || message.author.id !== UserStore.getCurrentUser().id) return;
-          e.ondblclick = () => MessageActions.startEditMessage(message.channel_id, message.id, message.content);
+        handleElement
+      ),
+      (() => {
+        function handle({ message }) {
+          setTimeout(() => {
+            if (message.author.id !== UserStore.getCurrentUser().id) return;
+            document.querySelectorAll(`#chat-messages-${message.channel_id}-${message.id}`).forEach(handleElement);
+          }, 1);
         }
-      )
-    )
+        FluxDispatcher.subscribe('MESSAGE_CREATE', handle);
+        return () => {
+          FluxDispatcher.unsubscribe('MESSAGE_CREATE', handle);
+        }
+      })()
+    );
+
   }
 }
