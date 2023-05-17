8fe01e4b9348502494f6014750e33c84108cb3ad
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2023-05-17 23:12:51 +0300

    audio presets plugin requires latest acord

---
+++ plugins/TheArmagan/audio-presets/src/index.js
@@ -0,0 +1,34 @@
+import { WebAudio } from "@acord/modules/custom";
+import { subscriptions, persist } from "@acord/extension";
+import { toasts } from "@acord/ui";
+import { logger } from "@acord/utils";
+import patcher from "@acord/patcher";
+
+export default {
+  load() {
+    subscriptions.push(
+      patcher.instead("_ensureAudio", WebAudio.prototype, async function (args, instead) {
+        let map = Object.fromEntries(persist.ghost.settings.preset.split(/;|\n/).map(i => i.trim().split("=")));
+
+        if (persist.ghost.settings.logAudioNames) {
+          logger.log(`[Sound Presets] Sound Name: ${this.name}`);
+          toasts.show.info(`[Sound Presets] Sound Name: ${this.name}`);
+        }
+
+        if (map[this.name]) {
+          let val = map[this.name];
+          if (!val.startsWith("https://")) {
+            this.name = val;
+            return instead.apply(this, args);
+          } else {
+            let a = await instead.apply(this, args);
+            a.src = val;
+            return a;
+          }
+        } else {
+          return instead.apply(this, args);
+        }
+      })
+    )
+  }
+}
