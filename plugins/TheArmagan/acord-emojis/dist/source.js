aef8a1d6b638fc7fb58fc638f27bfe5bd33fefa1
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2023-05-31 20:20:47 +0300

    acord emojis!!!

---
+++ plugins/TheArmagan/acord-emojis/dist/source.js
@@ -0,0 +1,111 @@
+(function (patcher, extension, common) {
+  'use strict';
+
+  function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+  var patcher__default = /*#__PURE__*/_interopDefaultLegacy(patcher);
+
+  var index = {
+    load() {
+      function ownToOg(content) {
+        const separator = "|(";
+        const closingSeparator = ")|";
+        let result = "";
+        let startIndex = 0;
+        let openingIndex, closingIndex;
+        while ((openingIndex = content.indexOf(separator, startIndex)) !== -1 && (closingIndex = content.indexOf(closingSeparator, openingIndex + separator.length)) !== -1) {
+          const animStr = content.charAt(openingIndex + separator.length) === "a" ? "a" : "";
+          const emoteStr = content.slice(openingIndex + separator.length + animStr.length, closingIndex);
+          const emoteParts = emoteStr.split(";");
+          if (emoteParts.length === 3 && emoteParts[1].trim() !== "") {
+            const emoteName = emoteParts[1].trim();
+            const emoteId = emoteParts[2];
+            result += content.slice(startIndex, openingIndex) + `<${animStr}:${emoteName}:${emoteId}>`;
+          } else {
+            result += content.slice(startIndex, closingIndex + closingSeparator.length);
+          }
+          startIndex = closingIndex + closingSeparator.length;
+        }
+        result += content.slice(startIndex);
+        return result.trim();
+      }
+      function ogToOwn(content) {
+        const separator = "<";
+        const closingSeparator = ">";
+        let result = "";
+        let startIndex = 0;
+        let openingIndex, closingIndex;
+        while ((openingIndex = content.indexOf(separator, startIndex)) !== -1 && (closingIndex = content.indexOf(closingSeparator, openingIndex + separator.length)) !== -1) {
+          const animStr = content.charAt(openingIndex + separator.length) === "a" ? "a" : "";
+          const emoteStr = content.slice(openingIndex + separator.length + animStr.length, closingIndex);
+          const emoteParts = emoteStr.split(":");
+          if (emoteParts.length === 3 && emoteParts[1].trim() !== "") {
+            const emoteName = emoteParts[1].trim();
+            const emoteId = emoteParts[2];
+            result += content.slice(startIndex, openingIndex) + `|(${animStr};${emoteName};${emoteId})|`;
+          } else {
+            result += content.slice(startIndex, closingIndex + closingSeparator.length);
+          }
+          startIndex = closingIndex + closingSeparator.length;
+        }
+        result += content.slice(startIndex);
+        return result.trim();
+      }
+      extension.subscriptions.push(
+        patcher__default["default"].before(
+          "sendMessage",
+          common.MessageActions,
+          function(args) {
+            if (args[1] && args[1].content) {
+              args[1].invalidEmojis = [];
+              args[1].validNonShortcutEmojis = args[1].validNonShortcutEmojis.filter((i) => !i.available);
+              args[1].content = ogToOwn(args[1].content);
+            }
+          }
+        ),
+        patcher__default["default"].before(
+          "editMessage",
+          common.MessageActions,
+          function(args) {
+            if (args[2] && args[2].content) {
+              args[2].content = ogToOwn(args[2].content);
+            }
+          }
+        ),
+        patcher__default["default"].instead(
+          "canUseEmojisEverywhere",
+          common.PremiumActions,
+          function(args, instead) {
+            return true;
+          }
+        ),
+        patcher__default["default"].instead(
+          "canUseAnimatedEmojis",
+          common.PremiumActions,
+          function(args, instead) {
+            return true;
+          }
+        ),
+        (() => {
+          function eventHandler(e) {
+            if ((e.type === "MESSAGE_CREATE" || e.type === "MESSAGE_UPDATE") && e.message && e.message.content) {
+              e.message.content = ownToOg(e.message.content);
+            } else if (e.type === "LOAD_MESSAGES_SUCCESS" && e.messages) {
+              e.messages.forEach((m) => {
+                if (m.content)
+                  m.content = ownToOg(m.content);
+              });
+            }
+          }
+          common.FluxDispatcher._interceptors.push(eventHandler);
+          return () => {
+            common.FluxDispatcher._interceptors.splice(common.FluxDispatcher._interceptors.indexOf(eventHandler), 1);
+          };
+        })()
+      );
+    }
+  };
+
+  return index;
+
+})($acord.patcher, $acord.extension, $acord.modules.common);
---
+++ plugins/TheArmagan/acord-emojis/src/index.js
@@ -0,0 +1,141 @@
+import patcher from "@acord/patcher";
+import { subscriptions } from "@acord/extension";
+import { MessageActions, PremiumActions, EmojiStore, SelectedGuildStore, FluxDispatcher } from "@acord/modules/common";
+
+export default {
+  load() {
+
+    // function ownToOg(content) {
+    //   return content.replace(
+    //     /\|\((a)?;?([^;]{2,})+;(\d+)\)\|/g,
+    //     (match, animStr, emoteName, emoteId) => {
+    //       if (!emoteName.trim()) return match;
+    //       return `<${animStr}:${emoteName}:${emoteId}>`
+    //     }
+    //   ).trim()
+    // }
+
+    // thanks to chat-gpt for this
+    function ownToOg(content) {
+      const separator = '|(';
+      const closingSeparator = ')|';
+
+      let result = '';
+      let startIndex = 0;
+      let openingIndex, closingIndex;
+
+      while ((openingIndex = content.indexOf(separator, startIndex)) !== -1 &&
+        (closingIndex = content.indexOf(closingSeparator, openingIndex + separator.length)) !== -1) {
+        const animStr = content.charAt(openingIndex + separator.length) === 'a' ? 'a' : '';
+        const emoteStr = content.slice(openingIndex + separator.length + animStr.length, closingIndex);
+        const emoteParts = emoteStr.split(';');
+        if (emoteParts.length === 3 && emoteParts[1].trim() !== '') {
+          const emoteName = emoteParts[1].trim();
+          const emoteId = emoteParts[2];
+          result += content.slice(startIndex, openingIndex) + `<${animStr}:${emoteName}:${emoteId}>`;
+        } else {
+          result += content.slice(startIndex, closingIndex + closingSeparator.length);
+        }
+
+        startIndex = closingIndex + closingSeparator.length;
+      }
+
+      result += content.slice(startIndex);
+      return result.trim();
+    }
+
+    // function ogToOwn(content) {
+    //   let selectedGuildId = SelectedGuildStore.getGuildId();
+    //   return content.replace(
+    //     /<(a)?:?([^:]{2,})+:(\d+)>/g,
+    //     (match, animStr, emoteName, emoteId) => {
+    //       if (!emoteName.trim()) return match;
+    //       let emoji = EmojiStore.getCustomEmojiById(emoteId);
+    //       if (emoji && !emoji.animated && selectedGuildId && emoji.guildId == selectedGuildId) return match;
+    //       return `|(${animStr};${emoteName};${emoteId})|`
+    //     }
+    //   ).trim();
+    // }
+
+    function ogToOwn(content) {
+      const separator = '<';
+      const closingSeparator = '>';
+
+      let result = '';
+      let startIndex = 0;
+      let openingIndex, closingIndex;
+
+      while ((openingIndex = content.indexOf(separator, startIndex)) !== -1 &&
+        (closingIndex = content.indexOf(closingSeparator, openingIndex + separator.length)) !== -1) {
+        const animStr = content.charAt(openingIndex + separator.length) === 'a' ? 'a' : '';
+        const emoteStr = content.slice(openingIndex + separator.length + animStr.length, closingIndex);
+        const emoteParts = emoteStr.split(':');
+        if (emoteParts.length === 3 && emoteParts[1].trim() !== '') {
+          const emoteName = emoteParts[1].trim();
+          const emoteId = emoteParts[2];
+          result += content.slice(startIndex, openingIndex) + `|(${animStr};${emoteName};${emoteId})|`;
+        } else {
+          result += content.slice(startIndex, closingIndex + closingSeparator.length);
+        }
+
+        startIndex = closingIndex + closingSeparator.length;
+      }
+
+      result += content.slice(startIndex);
+      return result.trim();
+    }
+
+
+    subscriptions.push(
+      patcher.before(
+        "sendMessage",
+        MessageActions,
+        function (args) {
+          if (args[1] && args[1].content) {
+            args[1].invalidEmojis = [];
+            args[1].validNonShortcutEmojis = args[1].validNonShortcutEmojis.filter(i => !i.available);
+            args[1].content = ogToOwn(args[1].content);
+          }
+        }
+      ),
+      patcher.before(
+        "editMessage",
+        MessageActions,
+        function (args) {
+          if (args[2] && args[2].content) {
+            args[2].content = ogToOwn(args[2].content);
+          }
+        }
+      ),
+      patcher.instead(
+        "canUseEmojisEverywhere",
+        PremiumActions,
+        function (args, instead) {
+          return true;
+        }
+      ),
+      patcher.instead(
+        "canUseAnimatedEmojis",
+        PremiumActions,
+        function (args, instead) {
+          return true;
+        }
+      ),
+      (() => {
+        function eventHandler(e) {
+          if ((e.type === "MESSAGE_CREATE" || e.type === "MESSAGE_UPDATE") && e.message && e.message.content) {
+            e.message.content = ownToOg(e.message.content);
+          } else if (e.type === "LOAD_MESSAGES_SUCCESS" && e.messages) {
+            e.messages.forEach(m => {
+              if (m.content) m.content = ownToOg(m.content);
+            });
+          }
+        }
+        FluxDispatcher._interceptors.push(eventHandler);
+        return () => {
+          FluxDispatcher._interceptors.splice(FluxDispatcher._interceptors.indexOf(eventHandler), 1);
+        }
+      })()
+    );
+  }
+}
