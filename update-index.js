1e154a0814a5e2e831c8d8d1e9c27ada70a9561a
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2023-07-27 13:03:39 +0300

    Update update-index.js

--- update-index.js
+++ update-index.js
@@ -8,8 +8,6 @@ function loadExtensions(type) {
     fs.readdirSync(path.resolve(__dirname, `./${type}`, authorDir.name)).forEach((extensionId) => {
       try {
         const cfg = JSON.parse(fs.readFileSync(path.resolve(__dirname, `./${type}`, authorDir.name, extensionId, "./acord.cfg"), "utf-8"));
-        delete cfg.config;
-        delete cfg.i18n;
         let outDir = cfg.out.directory.replace(/^(\.\/|\/)?/, "").replace(/\/$/, "");
         if (fs.existsSync(path.resolve(__dirname, `./${type}`, authorDir.name, extensionId, cfg.out.directory, "./source.js"))) {
           delete cfg.out;
@@ -18,7 +16,7 @@ function loadExtensions(type) {
               meta: {
                 author: authorDir.name,
                 id: extensionId,
-                url: `https://raw.githubusercontent.com/acord-standalone/verified-extensions/main/${type}/${authorDir.name}/${extensionId}/${outDir}`
+                url: `https://api.acord.app/extensions/raw/${type}/${authorDir.name}/${extensionId}/${outDir}`
               },
               manifest: cfg
             });
