f1416ac937c286877e4d29b29a87af98657e1406
ErdemGKSL <zahrakon06@gmail.com>
2023-08-15 15:57:02 +0300

    friendship url is back bois

--- plugins/ErdemGKSL/friend-url/src/index.js
+++ plugins/ErdemGKSL/friend-url/src/index.js
@@ -3,50 +3,60 @@ import { subscriptions, persist, i18n } from "@acord/extension";
 import injectSCSS from "./index.scss";
 import { InviteActions, UserStore } from "@acord/modules/common";
 import { notifications } from "@acord/ui";
-import { copyText } from "@acord/utils";
+import { copyText, react } from "@acord/utils";
 
-async function clickTrigger() {
-  const code = await getFriendCode();
-  if (!code) return notifications.show(i18n.format("ERROR_COPY"));
-  copyText(`https://discord.gg/${code}`);
-  notifications.show(i18n.format("SUCCESS_COPY"));
-}
+
 
 export default {
   load() {
-    subscriptions.push(dom.patch(`.userInfo-regn9W`, /**@param {Element} elm */(elm) => {
-      const packer = dom.parse(`<div class="acord--fu--packer"></div>`);
-      /** @type {Element} */
-      const btn = dom.parse(`<button class="acord--fu--button">${i18n.format("FRIEND_CODE_URL")}</button>`);
-      btn.addEventListener("click", clickTrigger);
+    async function clickTrigger() {
+      const code = await getFriendCode();
+      if (!code) return notifications.show(i18n.format("ERROR_COPY"));
+      copyText(`https://discord.gg/${code}`);
+      notifications.show(i18n.format("SUCCESS_COPY"));
+    }
+    async function getFriendCode() {
+      const userId = UserStore.getCurrentUser()?.id;
+      if (!userId) return null;
+      const oldCode = persist.ghost.oldCode?.[userId];
+      if (oldCode && oldCode.expires_at > Date.now()) {
+        return oldCode.code;
+      }
+    
+      const code = await InviteActions.createFriendInvite().catch(() => null);
+      if (!code) return null;
+    
+      if (code.inviter?.id) persist.store.oldCode[code.inviter?.id] = {
+        code: code.code,
+        expires_at: new Date(code.expires_at).getTime()
+      }
+      return code.code;
+    }
+    // subscriptions.push(dom.patch(`.userInfo-regn9W`, /**@param {Element} elm */(elm) => {
+    //   const packer = dom.parse(`<div class="acord--fu--packer"></div>`);
+    //   /** @type {Element} */
+    //   const btn = dom.parse(`<button class="acord--fu--button">${i18n.format("FRIEND_CODE_URL")}</button>`);
+    //   btn.addEventListener("click", clickTrigger);
 
-      const children = [...elm.children];
-      const editBtn = children.pop();
+    //   const children = [...elm.children];
+    //   const editBtn = children.pop();
 
-      packer.appendChild(editBtn);
-      packer.appendChild(btn);
+    //   packer.appendChild(editBtn);
+    //   packer.appendChild(btn);
       
-      elm.replaceChildren(...children, packer);
+    //   elm.replaceChildren(...children, packer);
+    // }));
+    subscriptions.push(dom.patch(`.relationshipButtons-3ByBpj`, /**@param {Element} elm */(elm) => {
+      const cUserId = (react.getProps(elm, (e) => e?.user))?.user?.id;
+      const userId = UserStore.getCurrentUser()?.id;
+      if (cUserId !== userId) return;
+      const button = dom.parse(`<button type="button" class="actionButton-iarQTd button-ejjZWC lookFilled-1H2Jvj colorGreen-jIPCAS sizeSmall-3R2P2p grow-2T4nbg"><div class="contents-3NembX">${i18n.format("FRIEND_CODE_URL")}</div></button>`);
+      button.addEventListener("click", clickTrigger);
+      elm.appendChild(button);
+      elm.appendChild(dom.parse(`<div class="acord--fu--spacing"></div>`));
     }));
     subscriptions.push(injectSCSS());
   },
   unload() { }
 }
 
-async function getFriendCode() {
-  const userId = UserStore.getCurrentUser()?.id;
-  if (!userId) return null;
-  const oldCode = persist.ghost.oldCode?.[userId];
-  if (oldCode && oldCode.expires_at > Date.now()) {
-    return oldCode.code;
-  }
-
-  const code = await InviteActions.createFriendInvite().catch(() => null);
-  if (!code) return null;
-
-  if (code.inviter?.id) persist.store.oldCode[code.inviter?.id] = {
-    code: code.code,
-    expires_at: new Date(code.expires_at).getTime()
-  }
-  return code.code;
-}
